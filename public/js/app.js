89f0d5a3a1e7f6ad91e44ef5a5caee79990f432a
lasni <grega.lasnibat@gmail.com>
2020-08-17 16:58:40 +0200

    Remove console.log calls

--- public/js/app.js
+++ public/js/app.js
@@ -13,10 +13,8 @@ weatherForm.addEventListener('submit', (e) => {
   fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        // console.log('error:', data.error);
         messageOne.textContent = data.error;
       } else {
-        // console.log('data:', data);
         const a = `Forecast for ${data.location}`;
         const b = `It's ${data.forecast} with ${data.temp}dgrC, feeling like ${data.feelslike}dgrC.\nHumidity: ${data.humidity}% / Pressure: ${data.pressure}hPa`;
         messageOne.textContent = a;
